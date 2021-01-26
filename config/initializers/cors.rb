7a0189f1b36af975eaedebcd2947d4f8bf2fe43c
timrinkerman <tim.rinkerman98@gmail.com>
2021-01-26 15:21:20 -0600

    deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         origins "http://localhost:3000"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+    allow do
+        origins "http://big-fridgy.herokuapp.com "
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
 end
