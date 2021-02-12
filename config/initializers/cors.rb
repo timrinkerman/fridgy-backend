d446d1d9e466106d0dc1bf918f3b9ef81e7e7a7d
timrinkerman <tim.rinkerman98@gmail.com>
2021-02-12 14:18:42 -0600

    added permissions for production that i was missing to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
+    allow do
+        origins "https://focused-jones-91254f.netlify.app"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
 end
